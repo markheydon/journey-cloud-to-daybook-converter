e1a3b1e2508ff377b9dc4d93b40fa501b4602cb8
Mark Heydon <markheydon@users.noreply.github.com>
2021-02-19 19:58:28 +0000

    Added DateTime and photo example.

--- JourneyCloudToDaybook.ConsoleApp/Program.cs
+++ JourneyCloudToDaybook.ConsoleApp/Program.cs
@@ -1,7 +1,9 @@
-﻿using static System.Console;
+﻿using System;
+using static System.Console;
 using System.IO;
 using JourneyCloudToDaybook.Library.Model;
 using System.Text.Json;
+using System.Linq;
 
 namespace JourneyCloudToDaybook.ConsoleApp
 {
@@ -9,12 +11,25 @@ namespace JourneyCloudToDaybook.ConsoleApp
     {
         static async System.Threading.Tasks.Task Main(string[] args)
         {
-            WriteLine("Hello World!");
-
             // Read in sample json file.
             using var stream = File.OpenRead(@"example.json");
             var journeyCloud = await JsonSerializer.DeserializeAsync<JourneyCloud>(stream);
-            WriteLine(journeyCloud.Text);
+            DateTime entryDateTime = DateTimeOffset
+                .FromUnixTimeMilliseconds(journeyCloud.DateJournal)
+                .UtcDateTime;
+            WriteLine(entryDateTime);
+            WriteLine(journeyCloud.PreviewText);
+            if(journeyCloud.Photos.Any())
+            {
+                foreach (var photo in journeyCloud.Photos)
+                {
+                    WriteLine($"Photo filename: {photo}.");
+                }
+            }
+            else
+            {
+                WriteLine(@"No photos found.");
+            }
         }
     }
 }
